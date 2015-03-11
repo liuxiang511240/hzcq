0cb455e47faf1883a6879e1c7d61ce4137e9db5b
tony.liu <tony.liu@rccchina.com>
2015-03-11 18:44:52 +0800

    header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Hzcq::Application.routes.draw do
+  root :controller => :home, :action => :index
+  resources :home
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
